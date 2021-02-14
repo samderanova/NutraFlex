b41f829433e63e5ed336d1a9f0d8ebb689c6313d
Sam Der <samder2468@gmail.com>
2021-02-13 20:40:09 -0800

    add random quote api

---
+++ lib/models/quote.dart
@@ -0,0 +1,47 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+/*
+Here is an example of how to use an API.
+In this scenario, I'm using an API to get random quotes (the randomization, by the way,
+is done by the API, not me).
+In order to make such requests, we'll need to import two things:
+
+- http (I already included this in the pubspec.yaml file, so there is no need to install it again)
+    Note that some packages must be installed by running `flutter pub get` in cmd, while others are already
+    included in Dart's standard library.
+- dart:convert (convert JSON response from the API to a dictionary, or a Map in Flutter)
+
+Once we have imported both, we can finally start making requests!
+*/
+
+// This is an asyncrhonous function that returns a Future object.
+Future fetchQuote() async {
+  // This async await part is like the promises Riley worked on for the backend
+  final response = await http.get('https://zenquotes.io/api/random');
+
+  if (response.statusCode == 200) {
+    // Return an instance from the Quote class
+    return Quote.fromJson(jsonDecode(response.body)[0]);
+  } else {
+    throw Exception(
+        'Bummer! Failed to retrieve an inspriational quote for you.');
+  }
+}
+
+class Quote {
+  final String quoteText;
+  final String author;
+  Quote({this.quoteText, this.author});
+
+  // Constructor for Quote class (specifically from JSON)
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    if (json['q'].startsWith('Too many requests')) {
+      throw Exception("Woah! We can't generate quotes for you this quickly. Please slow down.");
+    }
+    return Quote(
+      quoteText: json['q'],
+      author: json['a'],
+    );
+  }
+}
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/quote.dart';
 
 class HomePage extends StatefulWidget {
   final String name;
@@ -9,16 +10,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future futureQuote;
+
+  @override
+  // Initialize the state by fetching a quote from the API
+  void initState() {
+    super.initState();
+    futureQuote = fetchQuote();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Center(
-            child: Text('Welcome ${widget.name}'),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(10, 50, 10, 20),
+              child: Text(
+                'Hello, ${widget.name}!',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
           ),
+
+          // The Future object we got from fetching a quote is "built" here.
+          FutureBuilder(
+              future: futureQuote,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      Text(
+                        "An inspirational quote just for you!",
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.left,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(15),
+                              child: Text(
+                                snapshot.data.quoteText,
+                                style: TextStyle(
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '- ${snapshot.data.author}',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Center(
+                      child: Text(
+                        snapshot.error.toString().substring(11),
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                return CircularProgressIndicator();
+              })
         ],
-        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
       ),
     );
